d1e73d4fc1f4ed393f8d7c71a93c79a14734d6c1
psmarcin <ps@psmarcin.me>
2018-09-18 17:55:36 +0200

    fixup! Init server

--- server/server.go
+++ server/server.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+var port = ":8080"
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request %v", r.URL.Path)
 	fmt.Fprintf(w, "Reqest: %s", r.URL.Path)
@@ -13,5 +15,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func Start() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Starting server at %v", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
