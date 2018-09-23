838511eddb9c0ab539be8ec0141b16f1aa7271d6
psmarcin <ps@psmarcin.me>
2018-09-23 19:36:24 +0200

    fixup! Add Circleci integration

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-now -t ${NOWSH_TOKEN}
-now alias podcasts.psmarcin.me -t ${NOWSH_TOKEN}
-now rm podcasts --safe --yes -t ${NOWSH_TOKEN}
+now -t ${NOWSHTOKEN}
+now alias podcasts.psmarcin.me -t ${NOWSHTOKEN}
+now rm podcasts --safe --yes -t ${NOWSHTOKEN}
