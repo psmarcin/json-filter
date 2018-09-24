79108da2700168dc7d36343d400848d4ab124c19
psmarcin <ps@psmarcin.me>
2018-09-24 20:29:33 +0200

    fixup! fix: google api env variable name

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-now -t ${NOWSHTOKEN} -e PS_GOOGLE_API=@PS_GOOGLE_API
+now -t ${NOWSHTOKEN} -e PS_GOOGLE_API=@ps_google_api
 now alias podcasts.psmarcin.me -t ${NOWSHTOKEN}
 now rm podcasts.psmarcin.me --safe --yes -t ${NOWSHTOKEN}
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-now -e PS_GOOGLE_API=@PS_GOOGLE_API
+now -e PS_GOOGLE_API=@ps_google_api
 now alias podcasts.psmarcin.me
 now rm podcasts --safe --yes 
