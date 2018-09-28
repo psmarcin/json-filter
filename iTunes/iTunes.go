8a55e536b381e1e7eac33f45f5c347d803bed45f
Marcin <psmarcin@users.noreply.github.com>
2018-09-28 16:34:17 +0200

    v0.3 (#17)
    * fix: Docker image size and ssl certs (#7)
    
    * Create README.md (#8)
    
    * Update README.md
    
    * Improve iTunes feed (#15)
    
    * Proper xml declaration and closing
    
    * Add video type and hardcoded length
    
    * Add channel thumbnail and description
    
    * Adjust podcast feed
    
    * fixup! CircleCI integration (#6)
    
    * Fix ci (#16)

--- iTunes/iTunes.go
+++ iTunes/iTunes.go
@@ -17,24 +17,34 @@ type Feed struct {
 	Copyright   string   `xml:"copyright"`
 	XMLName     xml.Name `xml:"channel"`
 	PubDate     string   `xml:"pubDate"`
-	Item        []Item   `xml:"item"`
+	Category    string   `xml:"category"`
+	Image       struct {
+		Href string `xml:"href,attr"`
+	} `xml:"itunes:image"`
+	Item []Item `xml:"item"`
 }
 
 type Item struct {
+	GUID        string `xml:"guid"`
 	Title       string `xml:"title"`
-	Subtitle    string `xml:"itines:subtitle"`
-	ITitle      string `xml:"itunes:title"`
-	Author      string `xml:"itunes:author"`
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate     string `xml:"pubDate"`
 	Enclosure   struct {
-		URL  string `xml:"url,attr"`
-		Type string `xml:"type,attr"`
+		URL    string `xml:"url,attr"`
+		Type   string `xml:"type,attr"`
+		Length int    `xml:"length,attr"`
 	} `xml:"enclosure"`
-	Image struct {
+	Subtitle string `xml:"itines:subtitle"`
+	Image    struct {
 		Href string `xml:"href,attr"`
 	} `xml:"itunes:image"`
+	Duration    string `xml:"itunes:duration"`
+	Order       int    `xml:"itunes:order"`
+	ITitle      string `xml:"itunes:title"`
+	Summary     string `xml:"itunes:summary"`
+	Author      string `xml:"itunes:author"`
+	EpisodeType string `xml:"itunes:episodeType"`
 }
 
 type Owner struct {
@@ -48,13 +58,20 @@ func (f *Feed) ToXML() []byte {
 
 func Create(f feed.Feed) Feed {
 	feed := Feed{
-		Author:  f.Title,
-		Title:   f.Title,
-		Link:    f.Link.Href,
-		PubDate: f.Published,
+		Author:   f.Title,
+		Title:    f.Title,
+		Link:     f.Link.Href,
+		Category: "TV",
+		Owner: Owner{
+			Email: "p@pp.pp",
+		},
+		Language:    "en-us",
+		PubDate:     f.Published,
+		Description: f.ChannelDetails.Snippet.Description,
 	}
+	feed.Image.Href = f.ChannelDetails.Snippet.Thumbnails.High.URL
 	items := []Item{}
-	for _, v := range f.Feeds {
+	for i, v := range f.Feeds {
 		item := Item{
 			Title:       v.Title,
 			ITitle:      v.Title,
@@ -62,10 +79,17 @@ func Create(f feed.Feed) Feed {
 			PubDate:     v.Published,
 			Author:      v.Author,
 			Link:        v.Link.Href,
+			EpisodeType: "full",
+			Duration:    "11:32",
+			Order:       i,
+			GUID:        "pspod://" + f.ChannelID + "/" + v.YTID,
 			Description: v.Description,
+			Summary:     v.Description,
 		}
 		item.Image.Href = "https://i.ytimg.com/vi/" + v.YTID + "/maxresdefault.jpg"
-		item.Enclosure.URL = "http://podsync.net/download/PNyUU6D62/" + v.YTID + ".mp4"
+		item.Enclosure.URL = "http://podsync.net/download/PNyUU6D62/" + v.YTID + ".mp4?exp=tmp"
+		item.Enclosure.Type = "video/mp4"
+		item.Enclosure.Length = 242200000
 		items = append(items, item)
 	}
 	feed.Item = items
