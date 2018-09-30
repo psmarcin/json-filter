cc1db6e3367f600406819c228eeea4591efe5335
psmarcin <ps@psmarcin.me>
2018-09-30 16:28:03 +0200

    fixup! Fix: feed typo

--- iTunes/iTunes.go
+++ iTunes/iTunes.go
@@ -46,12 +46,10 @@ type Item struct {
 	Image    struct {
 		Href string `xml:"href,attr"`
 	} `xml:"itunes:image"`
-	Duration    string `xml:"itunes:duration"`
-	Order       int    `xml:"itunes:order"`
-	ITitle      string `xml:"itunes:title"`
-	Summary     string `xml:"itunes:summary"`
-	Author      string `xml:"itunes:author"`
-	EpisodeType string `xml:"itunes:episodeType"`
+	Order   int    `xml:"itunes:order"`
+	ITitle  string `xml:"itunes:title"`
+	Summary string `xml:"itunes:summary"`
+	Author  string `xml:"itunes:author"`
 }
 
 type Owner struct {
@@ -69,46 +67,45 @@ func Create(f feed.Feed) Feed {
 		log.Fatal("Time parse ", err)
 	}
 	feed := Feed{
-		Author:      f.Title,
 		Title:       f.Title,
-		Subtitle:    f.Title,
 		Link:        f.Link.Href,
+		Description: f.ChannelDetails.Snippet.Description,
 		Category:    "TV",
+		Author:      f.Title,
+		Subtitle:    f.Title,
 		Generator:   "psPodcast",
 		Language:    "en-us",
 		PubDate:     pub.Format(time.RFC1123Z),
-		Description: f.ChannelDetails.Snippet.Description,
 	}
 	feed.Image.URL = f.ChannelDetails.Snippet.Thumbnails.High.URL
 	feed.Image.Title = f.Title
 	feed.Image.Link = f.Link.Href
 	feed.ITunesImage.Href = f.ChannelDetails.Snippet.Thumbnails.High.URL
 	items := []Item{}
+
 	for i, v := range f.Feeds {
 		pub, err := time.Parse(time.RFC3339, v.Published)
 		if err != nil {
 			log.Fatal("Time parse ", err)
 		}
-		pubString := pub.Format(time.RFC1123Z)
 		item := Item{
+			GUID:        "pspod://" + f.ChannelID + "/" + v.YTID,
 			Title:       v.Title,
-			ITitle:      v.Title,
-			Subtitle:    v.Title,
-			PubDate:     pubString,
-			Author:      v.Author,
 			Link:        v.Link.Href,
-			EpisodeType: "full",
-			Duration:    "11:32",
-			Order:       i,
-			// GUID:        "pspod://" + f.ChannelID + "/" + v.YTID,
-			GUID:        "http://podsync.net/download/PNyUU6D62/" + v.YTID + ".mp4?exp=tmp",
 			Description: v.Description,
+			PubDate:     pub.Format(time.RFC1123Z),
+			Subtitle:    v.Title,
+			Order:       i,
+			ITitle:      v.Title,
 			Summary:     v.Description,
+			Author:      v.Author,
 		}
 		item.Image.Href = "https://i.ytimg.com/vi/" + v.YTID + "/maxresdefault.jpg"
+
 		item.Enclosure.URL = "http://podsync.net/download/PNyUU6D62/" + v.YTID + ".mp4?exp=tmp"
 		item.Enclosure.Type = "video/mp4"
 		item.Enclosure.Length = 242200000
+
 		items = append(items, item)
 	}
 	feed.Item = items
