82429e020217421d0af05abd268d2683016ef5d6
psmarcin <ps@psmarcin.me>
2018-09-30 17:24:37 +0200

    fixup! Feat: Add integration with New Relic

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -14,8 +13,4 @@ func Init() {
 	if err != nil {
 		log.Print("Can't load env variables ", err)
 	}
-
-	log.Print("PS_GOOGLE_API= ", os.Getenv("PS_GOOGLE_API"))
-	log.Print("PS_NEWRELIC_KEY= ", os.Getenv("PS_NEWRELIC_KEY"))
-	log.Print("PORT= ", os.Getenv("PORT"))
 }
