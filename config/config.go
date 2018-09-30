81bf24301a1056766cb40eb0c2e7f73690f60068
psmarcin <ps@psmarcin.me>
2018-09-30 17:15:37 +0200

    Feat: Add integration with New Relic

---
+++ config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Init load env variables from files
+func Init() {
+	log.SetPrefix("[CONFIG] ")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Can't load env variables ", err)
+	}
+
+	log.Print("PS_GOOGLE_API= ", os.Getenv("PS_GOOGLE_API"))
+	log.Print("PS_NEWRELIC_KEY= ", os.Getenv("PS_NEWRELIC_KEY"))
+	log.Print("PORT= ", os.Getenv("PORT"))
+}
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/psmarcin/youtubeGoesPodcast/server"
+import (
+	"github.com/psmarcin/youtubeGoesPodcast/config"
+	"github.com/psmarcin/youtubeGoesPodcast/server"
+)
+
+func init() {
+	config.Init()
+}
 
 func main() {
 	server.Start()
