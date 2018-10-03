e37de3f0be9637c43f95a295e2875458f761dcca
psmarcin <ps@psmarcin.me>
2018-10-03 16:39:53 +0200

    Feat: Generate feed url on hompage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,22 @@
 const inputElement = document.querySelector("#youtubeUrl")
 const exampleElement = document.querySelector("#exampleUrl")
+const generateElement = document.querySelector("#generate")
+const feedUrlElement = document.querySelector("#feedUrl")
+const feedResultElement = document.querySelector(".feedResult")
 const exampleUrl = exampleElement.innerHTML
-
+const BASE_URL = "http://podcast.psmarcin.me/feed?youtubeUrl="
 const fillExample = ()=>{
   inputElement.value = exampleUrl
 }
 
+const generateFeedUrl = (e)=>{
+  e.preventDefault()
+  const ytUrl = inputElement.value
+  const feedUrl = BASE_URL + encodeURIComponent(ytUrl)
+  feedUrlElement.value = feedUrl
+  feedResultElement.style.display = "block"
+}
+
+
 exampleElement.addEventListener("click", fillExample)
+generateElement.addEventListener("click", generateFeedUrl)
