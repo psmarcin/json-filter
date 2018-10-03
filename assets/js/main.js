f496526395c210ebeca89b62421c3ced36e44836
Marcin <psmarcin@users.noreply.github.com>
2018-10-03 16:48:12 +0200

    v0.6.1 (#28)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
 const inputElement = document.querySelector("#youtubeUrl")
 const exampleElement = document.querySelector("#exampleUrl")
-const exampleUrl = exampleElement.innerHTML
+const generateElement = document.querySelector("#generate")
+const feedUrlElement = document.querySelector("#feedUrl")
+const feedResultElement = document.querySelector(".feedResult")
+const formElement = document.querySelector("#form")
 
+const exampleUrl = exampleElement.innerHTML
+const BASE_URL = "http://podcast.psmarcin.me/feed?youtubeUrl="
 const fillExample = ()=>{
   inputElement.value = exampleUrl
 }
 
+const generateFeedUrl = (e)=>{
+  e.preventDefault()
+  const ytUrl = inputElement.value
+  const feedUrl = BASE_URL + encodeURIComponent(ytUrl)
+  feedUrlElement.value = feedUrl
+  feedResultElement.style.display = "block"
+}
+
+
 exampleElement.addEventListener("click", fillExample)
+formElement.addEventListener("submit", generateFeedUrl)
